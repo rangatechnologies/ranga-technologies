5ade1802896820b3d60ba014ddb587fb231a4224
Branganaupul <branganaupul@gmail.com>
2023-11-13 10:24:57 +0530

    browser logo change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/* <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/gh/lipis/flag-icons@6.6.6/css/flag-icons.min.css"
-      /> */}
+      <head>
+        <link rel="icon" type="image/png" href="/logo.png" />
+        <link rel="shortcut icon" type="image/png" href="/logo.png" />
+      </head>
       <body>{children}</body>
     </html>
   )
